4101a0a58729c76a65ffd04e99b12ce78c438869
minara35 <minara35@gmail.com>
2020-08-05 21:40:16 -0400

    add Method_Arrays.java

---
+++ src/utility/Method_Arrays.java
@@ -0,0 +1,9 @@
+package utility;
+import java.util.Arrays;
+
+public class Method_Arrays {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,34,6,};
+        System.out.println(Arrays.toString(arr));
+    }
+}
