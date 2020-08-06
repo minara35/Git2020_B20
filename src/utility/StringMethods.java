405f15744696b42a1a10b255fce276552b1642db
minara35 <minara35@gmail.com>
2020-08-05 21:03:41 -0400

    add StringMethods.java

---
+++ src/utility/StringMethods.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String str = "Minara";
+        if(str.contains("minara")){
+
+            System.out.println(str);
+        }
+    }
+}
