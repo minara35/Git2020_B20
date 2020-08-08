607615be51a8ad795d7fd7d4f9f0f0bdfa3ad491
minara35 <minara35@gmail.com>
2020-08-08 01:21:56 -0400

    add UniquesWord.java

--- src/utility/UniquesWords.java
+++ src/utility/UniquesWords.java
@@ -5,11 +5,12 @@ public class UniquesWords {
         String [] arr = {"C#", "Java", "Github", "C#", "Java"};
 
 for(String a : arr){
+    // String r = arr[0];  // C#
     String r = a;
     int count = 0;
     for(String each : arr){
-        if (r.equals(each)){
-            count++;
+        if (r.equals(each)){ // C#  == each words if true count will increase by one
+            count++; // C#2, Java2
         }
     }
     if(count == 1){
